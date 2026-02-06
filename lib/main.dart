1bcac66f667e1560f274955c39f179e8b77597c7
kleponijo <158453666+kleponijo@users.noreply.github.com>
2026-02-06 13:25:59 +0700

    update web firebase intialize guys

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:klimatologiot/simple_bloc_observer.dart';
 import 'package:klimatologiot/app.dart';
 import 'package:user_repository/user_repository.dart';
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = SimpleBlocObserver();
   runApp(MyApp(FirebaseUserRepo()));
 }
